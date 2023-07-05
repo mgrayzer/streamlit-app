053df85d6dcd6e06d7451bb46800cf09786fa130
mgrayzer <marlonagray@gmail.com>
2023-07-05 12:34:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & blueberry Oatmeal')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
 streamlit.text('Greek yogurt')
 streamlit.text('Chia seeds')
 streamlit.text('High protein chia pudding recipe')
