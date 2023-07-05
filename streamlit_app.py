c38e5673d35e34695a2fefda06b287c40ff6b75a
mgrayzer <marlonagray@gmail.com>
2023-07-05 14:03:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,6 @@ streamlit.text('Greek yogurt')
 streamlit.text('Chia seeds')
 streamlit.text('High protein chia pudding recipe')
 
+streamlit.header('Build Your Own Fruit Smoothie')
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
