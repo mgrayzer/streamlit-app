8b4bc220729ea0e96c7cabeee9ed80fa1ab30428
mgrayzer <marlonagray@gmail.com>
2023-07-05 12:30:16 +0200

    Update streamlit_app.py
    added breakfast list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry Oatmeal')
+streamlit.text('Greek yogurt')
+streamlit.text('Chia seeds')
+streamlit.text('High protein chia pudding recipe')
