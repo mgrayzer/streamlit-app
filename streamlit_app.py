618d98ef5595dad7452be5cf1fa6fa022539ce1e
mgrayzer <marlonagray@gmail.com>
2023-07-05 14:02:46 +0200

    Update streamlit_app.py
    Added csv list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -7,3 +8,6 @@ streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
 streamlit.text('Greek yogurt')
 streamlit.text('Chia seeds')
 streamlit.text('High protein chia pudding recipe')
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
