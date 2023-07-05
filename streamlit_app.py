41e1ddf03f6c8457c2850454021e1967d7da541a
mgrayzer <marlonagray@gmail.com>
2023-07-05 14:16:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,6 @@ streamlit.text('High protein chia pudding recipe')
 streamlit.header('Build Your Own Fruit Smoothie')
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick Some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruites_selected = streamlit.multiselect("Pick Some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruites_selected]
+streamlit.dataframe(fruits_to_show)
